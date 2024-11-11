4b3cf66bc90d4668e796ed6f4a12d1d4fc09fb44
Rivan <2010512022@mahasiswa.upnvj.ac.id>
2024-11-11 11:58:36 +0700

    update task 2

---
+++ task-2-my-dockerfile/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "<h1>“Eat, sleep, code, repeat!”</h1>")
+
+	fmt.Println("Someone hit me!")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	fmt.Println("Starting server on :80")
+	http.ListenAndServe(":80", nil)
+}
